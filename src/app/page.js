07067ea16cf3dc81330186e0235c33bc4036c23a
nocturnys <iamconstantinus@gmail.com>
2025-07-18 21:07:27 +0300

    responsive

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <main className="min-h-screen content-center">
-
-      <h10 className="flex select-none justify-center text-white text-9xl font-carol-gothic">
+    <main className="min-h-screen  content-center">
+      <h10 className=
+      "flex select-none justify-center text-white text-9xl max-sm:text-6xl font-carol-gothic">
         nocturnys
       </h10>
 
